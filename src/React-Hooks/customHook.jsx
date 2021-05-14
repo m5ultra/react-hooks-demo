2122e599da353eaa5ad7415f0c08d56f425e0b04
Ellis <352671309@qq.com>
2021-05-14 16:20:11 +0800

    Update customHook.jsx

--- src/React-Hooks/customHook.jsx
+++ src/React-Hooks/customHook.jsx
@@ -1,1 +1,27 @@
 
+import { useState } from 'react'
+import { Typography, Button } from 'antd'
+
+const { Title } = Typography
+const useCus = (val, num) => {
+  const [count, setCount] = useState(val)
+  const add = () => {
+    setCount(() => count + num)
+  }
+  return [count, add]
+}
+//
+const UseCustomHooks = () => {
+  const [count, add] = useCus(10, 2)
+  return (
+    <>
+      <Title>CustomHook</Title>
+      <Title level={3}>
+        {count}
+      </Title>
+      <Button onClick={() => add()}>Add</Button>
+    </>
+  )
+}
+
+export default UseCustomHooks
