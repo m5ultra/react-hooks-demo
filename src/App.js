de4564a4ffe984f035fdaa62ca6134426413d95d
Ellis <352671309@qq.com>
2021-05-14 10:39:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import UseEffect from './React-Hooks/use-effect'
 import UseRef from './React-Hooks/use-ref'
 import UseContext from './React-Hooks/use-context'
 import UseMemo from './React-Hooks/use-memo'
+import UseCallback from './React-Hooks/use-callback'
 import './React-Hooks/style.less'
 import { Typography } from 'antd'
 
@@ -22,12 +23,14 @@ function App() {
         <Button type={'primary'} size={'large'}><Link to="/use-ref">Use Ref</Link></Button>
         <Button type={'primary'} size={'large'}><Link to="/use-context">Use Context</Link></Button>
         <Button type={'primary'} size={'large'}><Link to="/use-memo">Use Memo</Link></Button>
+        <Button type={'primary'} size={'large'}><Link to="/use-callback">Use Callback</Link></Button>
         <div>
           <Route path="/" exact component={UseState}/>
           <Route path="/use-effect" component={UseEffect}/>
           <Route path="/use-ref" component={UseRef}/>
           <Route path="/use-context" component={UseContext}/>
           <Route path="/use-memo" component={UseMemo}/>
+          <Route path="/use-callback" component={UseCallback}/>
         </div>
       </Router>
     </div>
