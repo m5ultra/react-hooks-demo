64dfd498ddf39b1c0b743ea85b83e534c0d289a6
Dendi <352671309@qq.com>
2021-05-14 22:10:58 +0800

    chore: add TODO

--- src/React-Hooks/useReducer/CusRedux.js
+++ src/React-Hooks/useReducer/CusRedux.js
@@ -2,6 +2,10 @@ import ComA from './comA'
 import ComB from './comB'
 import Reducer from './reducer'
 // TODO reducer 拆分 模拟combinerReducer
+// combineReducer https://stackoverflow.com/questions/59200785/react-usereducer-how-to-combine-multiple-reducers
+// 第三方库 https://www.npmjs.com/package/react-combine-reducers
+// 不支持插件
+// 没有调试工具 类似redux-dev-tools
 const CusRedux = () => {
   return (
     <>
