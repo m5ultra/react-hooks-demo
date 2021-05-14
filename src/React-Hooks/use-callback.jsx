88e650daed7f4fcab73baee58169289d580a018a
Ellis <352671309@qq.com>
2021-05-14 10:40:41 +0800

    Create use-callback.jsx

---
+++ src/React-Hooks/use-callback.jsx
@@ -0,0 +1,14 @@
+import { useCallback } from 'react'
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+function UseCallback() {
+  return (
+    <>
+      <Title>useCallback</Title>
+    </>
+  )
+}
+
+export default UseCallback
