f068806cee6426cba78e2e0825c7b584fb193d3e
Ellis <352671309@qq.com>
2021-05-14 10:53:35 +0800

    Update use-callback.jsx

--- src/React-Hooks/use-callback.jsx
+++ src/React-Hooks/use-callback.jsx
@@ -1,12 +1,27 @@
-import { useCallback } from 'react'
-import { Typography } from 'antd'
+import { useCallback, useState } from 'react'
+import { Typography, Button } from 'antd'
 
 const { Title } = Typography
 
 function UseCallback() {
+  const [count, setCount] = useState(9)
+  const [num, setNum] = useState(10)
+  const cb = useCallback(() => {
+    console.log(count)
+    return count
+  }, [num])
   return (
     <>
-      <Title>useCallback</Title>
+      <Title level={1}>useCallback</Title>
+      <br/>
+      <Title level={3}>{count}</Title>
+      <Title level={3}>{cb()}</Title>
+      <Button onClick={() => {
+        setCount(count + 1)
+      }}>setCount</Button>
+      <Button onClick={() =>
+        setNum(num + 1)
+      }>setNum</Button>
     </>
   )
 }
