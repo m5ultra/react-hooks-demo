da733ad9f3b987187a736aa2600cb9ec5695b912
Ellis <352671309@qq.com>
2021-05-14 11:05:54 +0800

    Create use-imperative-handle.jsx

---
+++ src/React-Hooks/use-imperative-handle.jsx
@@ -0,0 +1,12 @@
+import { useImperativeHandle } from 'react'
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+function UseImperativeHandle() {
+  return (<>
+    <Title>useImperativeHandle</Title>
+  </>)
+}
+
+export default UseImperativeHandle
