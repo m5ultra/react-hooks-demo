5e1cc4ff681298a8e3d9f968340d937b646cb00e
Ellis <352671309@qq.com>
2021-05-19 17:36:55 +0800

    Update craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -4,15 +4,54 @@
 // craco https://cloud.tencent.com/developer/article/1749704
 // 移动端适配方案 1.https://bubble93.github.io/2021/03/11/%E7%A7%BB%E5%8A%A8%E7%AB%AF%E9%80%82%E9%85%8D%E6%96%B9%E6%A1%88/
 // 移动端适配方案 2.https://www.cnblogs.com/zhangnan35/p/12682925.html
+// 移动端适配方案 3.https://www.cnblogs.com/yikuu/p/9052148.html 大漠
+// umi 适配视口 4.https://www.jianshu.com/p/9b9cdba7ab21
 const CracoLessPlugin = require('craco-less')
 const postcssAspectRatioMini = require('postcss-aspect-ratio-mini')
 const postcssWriteSvg = require('postcss-write-svg')
 const px2ViewPort = require('postcss-px-to-viewport')
+// const postcssImport = require('postcss-import')
+// const postcssUrl = require('postcss-url')
+// const postcsscssnext = require('postcss-cssnext')
+// const cssnano = require('cssnano')
 const path = require('path')
 const pathResolve = pathUrl => path.join(__dirname, pathUrl)
 module.exports = {
   style: {
     postcss: {
+      // plugins: [
+      //   postcssImport({}),
+      //   postcssUrl({}),
+      //   postcssAspectRatioMini({}),
+      //   postcssWriteSvg({ utf8: false }),
+      //   postcsscssnext({}),
+      //   px2ViewPort(
+      //     {
+      //       unitToConvert: 'px', // 默认值`px`，需要转换的单位
+      //       viewportWidth: 750, // 视窗的宽度,对应设计稿宽度
+      //       viewportHeight: 1334, // 视窗的高度, 根据375设备的宽度来指定，一般是667，也可不配置
+      //       unitPrecision: 10, // 指定`px`转换为视窗单位值的小数位数
+      //       propList: ['*'], // 转化为vw的属性列表
+      //       viewportUnit: 'vw', // 指定需要转换成视窗单位
+      //       fontViewportUnit: 'vw', // 字体使用的视窗单位
+      //       selectorBlaskList: ['.ignore-'], // 指定不需要转换为视窗单位的类
+      //       mediaQuery: false, // 允许在媒体查询中转换`px`
+      //       minPixelValue: 1, // 小于或等于`1px`时不转换为视窗单位
+      //       replace: false, // 是否直接更换属性值而不添加备用属性
+      //       exclude: /(\/|\\)(node_modules)(\/|\\)/, // 忽略某些文件夹下的文件或特定文件
+      //       landscape: false, // 是否添加根据landscapeWidth生成的媒体查询条件 @media (orientation: landscape)
+      //       landscapeUnit: 'vw', // 横屏时使用的单位
+      //       landscapeWidth: 1134 // 横屏时使用的视窗宽度
+      //     }
+      //   ),
+      //   cssnano({
+      //     preset: 'advanced',
+      //     autoprefixer: false,
+      //     'postcss-zindex': false,
+      //     zindex: false
+      //   })
+      // ],
+
       loaderOptions: {
         ident: 'postcss',
         plugins: () => [
@@ -39,7 +78,7 @@ module.exports = {
           ),
         ],
       },
-    }
+    },
   },
   webpack: {
     alias: {
